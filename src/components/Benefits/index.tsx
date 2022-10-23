6716717c88c71617a124a69039cf4cdb4482318c
paulorobinson <paulo.giaciani@gmail.com>
2022-10-22 23:13:55 -0300

    Add Link and Image

--- src/components/Benefits/index.tsx
+++ src/components/Benefits/index.tsx
@@ -1,35 +1,46 @@
 import Card from '../Card';
+import Divider from '../Divider';
+import Link from '../Link';
 import styles from './styles.module.scss';
 
 const Benefits = () => {
   return (
-    <section className={styles.container}>
-      <Card
-        image="./src/assets/images/qualidade.png"
-        title="Qualidade de vida"
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
+    <>
+      <section className={styles.container}>
+        <Card
+          image="./src/assets/images/qualidade.png"
+          title="Qualidade de vida"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
         officiis ipsa corrupti necessitatibus similique soluta adipisci
         doloremque ipsam perspiciatis, facilis voluptas quae? Ipsam soluta
         magnam necessitatibus assumenda cum cumque est."
-      />
-      <Card
-        image="./src/assets/images/descontracao.png"
-        title="Ambiente Descontraído"
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
+        />
+        <Card
+          image="./src/assets/images/descontracao.png"
+          title="Ambiente Descontraído"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
         officiis ipsa corrupti necessitatibus similique soluta adipisci
         doloremque ipsam perspiciatis, facilis voluptas quae? Ipsam soluta
         magnam necessitatibus assumenda cum cumque est.Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
         officiis ipsa corrupti necessitatibus similique."
-      />
-      <Card
-        image="./src/assets/images/atividades.png"
-        title="Atividades extras"
-        description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
+        />
+        <Card
+          image="./src/assets/images/atividades.png"
+          title="Atividades extras"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Libero
         officiis ipsa corrupti necessitatibus similique soluta adipisci
         doloremque ipsam perspiciatis, facilis voluptas quae? Ipsam soluta
         magnam necessitatibus assumenda cum cumque est."
+        />
+      </section>
+      <Divider />
+      <Link href="https://elo7.gupy.io/">Saiba mais</Link>
+      <img
+        className={styles.imageBottom}
+        src="./src/assets/images/foto-bottom.png"
+        alt=""
       />
-    </section>
+    </>
   );
 };
 
