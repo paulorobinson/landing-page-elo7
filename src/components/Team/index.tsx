84665c743475a69397a43f7174b682fa983c86a2
paulorobinson <paulo.giaciani@gmail.com>
2022-10-22 22:27:40 -0300

    Create Team component

---
+++ src/components/Team/index.tsx
@@ -0,0 +1,39 @@
+import Title from '../Title';
+import styles from './styles.module.scss';
+
+const Team = () => {
+  return (
+    <section className={styles.container}>
+      <div className={styles.about}>
+        <a
+          href="https://www.youtube.com/watch?v=d0LaoRbuJSw&ab_channel=CortesdoDigitalizePodcast"
+          target="_blank"
+        >
+          <img src="./src/assets/images/placeholder-video.png" alt="" />
+        </a>
+
+        <div className={styles.aboutDescription}>
+          <h1>Palavra do CEO</h1>
+          <h2>Carlos Curioni</h2>
+          <p>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eum autem
+            temporibus quasi molestiae delectus optio rerum nihil amet. Sunt
+            impedit non cupiditate quos, deserunt nobis deleniti ducimus
+            possimus animi reiciendis!
+          </p>
+        </div>
+      </div>
+      <Title>
+        Conheça nosso time <br /> fora de série
+      </Title>
+      <div className={styles.teamImages}>
+        <img src="./src/assets/images/camila.png" alt="" />
+        <img src="./src/assets/images/guto.png" alt="" />
+        <img src="./src/assets/images/david.png" alt="" />
+        <img src="./src/assets/images/beatriz.png" alt="" />
+      </div>
+    </section>
+  );
+};
+
+export default Team;
